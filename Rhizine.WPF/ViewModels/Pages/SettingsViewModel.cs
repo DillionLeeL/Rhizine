91e411a305c3dc3e1428d76794379c95be6a1d9f
DillionLowry <dillionleel@gmail.com>
2023-12-25 11:56:05 -0600

    SettingsViewModel

---
+++ Rhizine.WPF/ViewModels/Pages/SettingsViewModel.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MahApps.Metro.Controls;
+using Microsoft.Extensions.Options;
+using Rhizine.Core.Models;
+using Rhizine.Core.Services.Interfaces;
+using Rhizine.Core.ViewModels;
+using Rhizine.WPF.Models;
+using Rhizine.WPF.Properties;
+
+namespace Rhizine.Displays.Pages;
+
+public partial class SettingsViewModel : BaseViewModel
+{
+    private readonly AppConfig _appConfig;
+    private readonly IThemeSelectorService _themeSelectorService;
+    private readonly ISystemService _systemService;
+    private readonly IApplicationInfoService _applicationInfoService;
+    private readonly ILoggingService _loggingService;
+
+    [ObservableProperty]
+    private AppTheme _theme;
+
+    [ObservableProperty]
+    private string _versionDescription;
+
+    [ObservableProperty]
+    private FlyoutsControl _flyoutsControl;
+
+    [ObservableProperty]
+    private Flyout _testFlyout;
+
+    public SettingsViewModel(IOptions<AppConfig> appConfig, IThemeSelectorService themeSelectorService, ISystemService systemService,
+                             IApplicationInfoService applicationInfoService, ILoggingService loggingService)
+    {
+        _appConfig = appConfig?.Value ?? new AppConfig();
+        _themeSelectorService = themeSelectorService;
+        _systemService = systemService;
+        _applicationInfoService = applicationInfoService;
+        _loggingService = loggingService;
+    }
+
+    public override async Task OnNavigatedTo(object parameter)
+    {
+        VersionDescription = $"{Resources.AppDisplayName} - {_applicationInfoService.GetVersion()}";
+        Theme = _themeSelectorService.GetCurrentTheme();
+    }
+
+
+    [RelayCommand]
+    private void SetTheme(string themeName)
+    {
+        var theme = (AppTheme)Enum.Parse(typeof(AppTheme), themeName);
+        _themeSelectorService.SetTheme(theme);
+    }
+
+    //[RelayCommand]
+    //private void PrivacyStatement() => _systemService.OpenInWebBrowser(_appConfig.PrivacyStatement);
+}
