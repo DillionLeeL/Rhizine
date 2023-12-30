241bd716a7c33e3dda4585e7438ba8cc87c7e3f5
DillionLowry <dillionleel@gmail.com>
2023-12-30 10:03:32 -0600

    Core INavigationAware init

---
+++ Rhizine.Core/ViewModels/INavigationAware.cs
@@ -0,0 +1,9 @@
+﻿namespace Rhizine.Core.Models.Interfaces;
+
+public interface INavigationAware
+{
+    // TODO: async navigation, not void, with default implementation
+    void OnNavigatedTo(object parameter);
+
+    void OnNavigatedFrom();
+}
