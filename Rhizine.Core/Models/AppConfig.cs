21473b086305a483c3c11b0d103bffbbfad9db12
DillionLowry <dillionleel@gmail.com>
2023-12-30 10:02:40 -0600

    Core settings models init

---
+++ Rhizine.Core/Models/AppConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace Rhizine.Models;
+
+// TODO
+public class AppConfig
+{
+    public string ConfigurationsFolder { get; set; }
+
+    public string AppPropertiesFileName { get; set; }
+
+    public string PrivacyStatement { get; set; }
+
+    public bool SingleFilePublish { get; set; } = true;
+
+    public string UserFileName { get; set; }
+
+    public string IdentityClientId { get; set; }
+
+    public string IdentityCacheFileName { get; set; }
+
+    public string IdentityCacheDirectoryName { get; set; }
+}
