bd5e69bb33886fd6a2839a17143f97a8da206101
DillionLowry <dillionleel@gmail.com>
2023-12-30 10:03:58 -0600

    WinUI service interfaces init

---
+++ Rhizine.WinUI/Services/Interfaces/IAppNotificationService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Specialized;
+
+namespace Rhizine.WinUI.Services.Interfaces;
+
+public interface IAppNotificationService
+{
+    void Initialize();
+
+    bool Show(string payload);
+
+    NameValueCollection ParseArguments(string arguments);
+
+    void Unregister();
+}
