5dffa618e211b1f63fee04dcf53d20afebbe3392
DillionLowry <dillionleel@gmail.com>
2024-01-01 12:53:14 -0600

    CRL dependency injection issue fix

--- Rhizine.WinUI/ViewModels/ContentGridDetailViewModel.cs
+++ Rhizine.WinUI/ViewModels/ContentGridDetailViewModel.cs
@@ -1,21 +1,22 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-using Rhizine.WinUI.Contracts.ViewModels;
-using Rhizine.Core.Services.Interfaces;
 using Rhizine.Core.Models;
+using Rhizine.Core.Models.Interfaces;
+using Rhizine.Core.Services.Interfaces;
+using Rhizine.Core.ViewModels;
 
 namespace Rhizine.WinUI.ViewModels;
 
 public partial class ContentGridDetailViewModel : ObservableRecipient, INavigationAware
 {
     private readonly ISampleDataService _sampleDataService;
-
+    public readonly INavigationService NavigationService;
     [ObservableProperty]
     private SampleOrder? item;
 
-    public ContentGridDetailViewModel(ISampleDataService sampleDataService)
+    public ContentGridDetailViewModel(ISampleDataService sampleDataService, INavigationService<Microsoft.UI.Xaml.Controls.Frame, Microsoft.UI.Xaml.Navigation.NavigationEventArgs> navigationService)
     {
         _sampleDataService = sampleDataService;
+        NavigationService = navigationService;
     }
 
     public async void OnNavigatedTo(object parameter)
