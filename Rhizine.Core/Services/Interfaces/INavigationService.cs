99faec58a6bff3076d5663730c96ce28cdb7814a
DillionLowry <dillionleel@gmail.com>
2024-01-04 20:51:22 -0600

    WPF navigation service, core INavigationService

--- Rhizine.Core/Services/Interfaces/INavigationService.cs
+++ Rhizine.Core/Services/Interfaces/INavigationService.cs
@@ -1,10 +1,18 @@
 ﻿namespace Rhizine.Core.Services.Interfaces;
 
-public interface INavigationService<TEventArgs> where TEventArgs : EventArgs
+public interface INavigationService<TNavigationSource, TEventArgs>
 {
+    TNavigationSource NavigationSource { get; }
+
     event EventHandler<TEventArgs> Navigated;
 
-    void Initialize(object? parameter = null);
+    bool CanGoBack { get; }
+
+    void Initialize(TNavigationSource source);
+
+    bool NavigateTo(string pageKey, object? parameter = null, bool clearNavigation = false);
+
+    bool GoBack();
 
     Task NavigateToAsync(string pageKey, object? parameter = null, bool clearNavigation = false);
 
