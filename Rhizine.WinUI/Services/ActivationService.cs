be09f3a4052f6521f6100a02bc07b5e3c5397e97
DillionLowry <dillionleel@gmail.com>
2024-01-08 18:17:32 -0600

    WinUI Activation service updates

--- Rhizine.WinUI/Services/ActivationService.cs
+++ Rhizine.WinUI/Services/ActivationService.cs
@@ -5,8 +5,6 @@ using Rhizine.WinUI.Activation;
 using Rhizine.WinUI.Services.Interfaces;
 using Rhizine.WinUI.ViewModels;
 using Rhizine.WinUI.Views;
-using IPageService = Rhizine.Core.Services.Interfaces.IPageService<Microsoft.UI.Xaml.Controls.Page>;
-using IThemeSelectorService = Rhizine.Core.Services.Interfaces.IThemeSelectorService;
 
 namespace Rhizine.WinUI.Services;
 
@@ -16,37 +14,47 @@ public class ActivationService : IActivationService
     private readonly IEnumerable<IActivationHandler> _activationHandlers;
     private readonly IThemeSelectorService _themeSelectorService;
     private readonly IPageService _pageService;
+    private readonly ILoggingService _loggingService;
     private UIElement? _shell = null;
 
-    public ActivationService(ActivationHandler<LaunchActivatedEventArgs> defaultHandler, IEnumerable<IActivationHandler> activationHandlers, 
-        IThemeSelectorService themeSelectorService, IPageService pageService)
+    public ActivationService(ActivationHandler<LaunchActivatedEventArgs> defaultHandler, IEnumerable<IActivationHandler> activationHandlers,
+        IThemeSelectorService themeSelectorService, IPageService pageService, ILoggingService loggingService)
     {
         _defaultHandler = defaultHandler;
         _activationHandlers = activationHandlers;
         _themeSelectorService = themeSelectorService;
         _pageService = pageService;
+        _loggingService = loggingService;
     }
 
     public async Task ActivateAsync(object activationArgs)
     {
-        // Execute tasks before activation.
-        await InitializeAsync();
-
-        // Set the MainWindow Content.
-        if (App.MainWindow.Content == null)
+        try
         {
-            _shell = App.GetService<ShellPage>();
-            App.MainWindow.Content = _shell ?? new Frame();
+            // Execute tasks before activation.
+            await InitializeAsync();
+
+            // Set the MainWindow Content.
+            if (App.MainWindow.Content == null)
+            {
+                _shell = App.GetService<ShellPage>();
+                App.MainWindow.Content = _shell ?? new Frame();
+            }
+
+            // Handle activation via ActivationHandlers.
+            await HandleActivationAsync(activationArgs);
+
+            // Activate the MainWindow.
+            App.MainWindow.Activate();
+
+            // Execute tasks after activation.
+            await StartupAsync();
         }
-
-        // Handle activation via ActivationHandlers.
-        await HandleActivationAsync(activationArgs);
-
-        // Activate the MainWindow.
-        App.MainWindow.Activate();
-
-        // Execute tasks after activation.
-        await StartupAsync();
+        catch (Exception ex)
+        {
+            await _loggingService.LogErrorAsync(ex);
+            throw;
+        }
     }
 
     private async Task HandleActivationAsync(object activationArgs)
@@ -66,7 +74,6 @@ public class ActivationService : IActivationService
 
     private async Task InitializeAsync()
     {
-        await _themeSelectorService.InitializeAsync().ConfigureAwait(false);
         _pageService.Register<MainViewModel, MainPage>();
         _pageService.Register<WebViewViewModel, WebViewPage>();
         _pageService.Register<DataGridViewModel, DataGridPage>();
@@ -74,13 +81,13 @@ public class ActivationService : IActivationService
         _pageService.Register<ContentGridDetailViewModel, ContentGridDetailPage>();
         _pageService.Register<ListDetailsViewModel, ListDetailsPage>();
         _pageService.Register<SettingsViewModel, SettingsPage>();
-        await Task.CompletedTask;
+        
+        await _themeSelectorService.InitializeAsync().ConfigureAwait(false);
     }
 
+    // Empty for now
     private async Task StartupAsync()
     {
-        // TODO
-        //await _themeSelectorService.SetThemeAsync();
         await Task.CompletedTask;
     }
 }
